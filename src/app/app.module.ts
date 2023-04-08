d4e7bf06d4ecfe8d9ea95739d26713caab6f16c0
Connor Brady <connorbrady@codox.eu>
2023-04-08 21:55:47 +0100

    WIP Adding OIDC Library

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,14 @@
  * Copyright Akveo. All Rights Reserved.
  * Licensed under the MIT License. See License.txt in the project root for license information.
  */
-import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
-import { CoreModule } from './@core/core.module';
-import { ThemeModule } from './@theme/theme.module';
-import { AppComponent } from './app.component';
-import { AppRoutingModule } from './app-routing.module';
+import {BrowserModule} from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {NgModule} from '@angular/core';
+import {HttpClientModule} from '@angular/common/http';
+import {CoreModule} from './@core/core.module';
+import {ThemeModule} from './@theme/theme.module';
+import {AppComponent} from './app.component';
+import {AppRoutingModule} from './app-routing.module';
 import {
   NbChatModule,
   NbDatepickerModule,
@@ -20,7 +20,7 @@ import {
   NbToastrModule,
   NbWindowModule,
 } from '@nebular/theme';
-import {AuthModule} from "angular-auth-oidc-client";
+import {AuthModule, LogLevel} from 'angular-auth-oidc-client';
 
 @NgModule({
   declarations: [AppComponent],
@@ -41,7 +41,17 @@ import {AuthModule} from "angular-auth-oidc-client";
     CoreModule.forRoot(),
     ThemeModule.forRoot(),
     AuthModule.forRoot({
-
+      config: {
+        authority: 'http://localhost:8080/realms/awms',
+        redirectUrl: window.location.origin,
+        postLogoutRedirectUri: window.location.origin,
+        clientId: 'awms-web-client',
+        scope: 'openid profile email offline_access',
+        responseType: 'code',
+        silentRenew: true,
+        useRefreshToken: true,
+        logLevel: LogLevel.Debug,
+      },
     }),
   ],
   bootstrap: [AppComponent],
