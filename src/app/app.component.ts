13881a83fcb5bc58ea7d4044c8fd42518fb990f1
Connor Brady <connorbrady@codox.eu>
2023-04-08 22:21:32 +0100

    UPDATED Logic to point to auth service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,9 +4,10 @@
  * Licensed under the MIT License. See License.txt in the project root for license information.
  */
 import { Component, OnInit } from '@angular/core';
-import { OidcSecurityService } from 'angular-auth-oidc-client';
+import {LoginResponse, OidcSecurityService} from 'angular-auth-oidc-client';
 import { AnalyticsService } from './@core/utils/analytics.service';
 import { SeoService } from './@core/utils/seo.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'ngx-app',
@@ -18,13 +19,16 @@ export class AppComponent implements OnInit {
     private oidcSecurityService: OidcSecurityService,
     private analytics: AnalyticsService,
     private seoService: SeoService,
+    private readonly router: Router,
   ) {
   }
 
   ngOnInit(): void {
-    this.oidcSecurityService.checkAuth().subscribe((auth) => {
-
-      console.log('is authenticated', auth);
+    this.oidcSecurityService.checkAuth().subscribe((auth: LoginResponse) => {
+      if (!auth.isAuthenticated) {
+        this.oidcSecurityService.authorize();
+        // this.router.navigate(['/auth/login']);
+      }
     });
 
     this.analytics.trackPageViews();
