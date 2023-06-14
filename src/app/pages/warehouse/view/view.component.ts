989d65fbe6feb386a86d33f754fc6429763f7128
Connor Brady <connorbrady@codox.eu>
2023-06-14 21:57:49 +0100

    ADDED View warehouses component

--- src/app/pages/warehouse/view/view.component.ts
+++ src/app/pages/warehouse/view/view.component.ts
@@ -1,4 +1,9 @@
 import {Component, OnDestroy, OnInit} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {NbWindowService} from '@nebular/theme';
+import {Router} from '@angular/router';
+import {ServerDataSource} from 'ng2-smart-table';
+import {getFilterableTableSettings} from '../../../../shared/filterable-table-settings';
 
 @Component({
   selector: 'ngx-warehouse-view',
@@ -6,7 +11,56 @@ import {Component, OnDestroy, OnInit} from '@angular/core';
   templateUrl: './view.component.html',
 })
 export class ViewComponent implements OnDestroy, OnInit {
+  constructor(
+    private readonly httpClient: HttpClient,
+    private readonly windowService: NbWindowService,
+    private readonly router: Router,
+  ) {}
 
+  settings = {
+    actions: {
+      custom: [
+        {
+          name: 'view',
+          title: '<img src="assets/icons/outline/eye-outline.svg" width="20" height="20" >',
+        },
+        {
+          name: 'edit',
+          title: '<img src="assets/icons/outline/edit-2-outline.svg" width="20" height="20" >',
+        },
+        {
+          name: 'delete',
+          title: '<img src="assets/icons/outline/trash-2-outline.svg" width="20" height="20" >',
+        },
+      ],
+      add: false,
+      edit: false,
+      delete: false,
+    },
+    columns: {
+      name: {
+        title: 'Name',
+        type: 'string',
+      },
+      code: {
+        title: 'Code',
+        type: 'string',
+      },
+      active: {
+        title: 'Active',
+        type: 'boolean',
+      },
+      contactTelephone: {
+        title: 'Contact Telephone',
+        type: 'string',
+      },
+    },
+  };
+
+  source: ServerDataSource = new ServerDataSource(
+    this.httpClient,
+    getFilterableTableSettings('warehouse'),
+  );
 
   ngOnDestroy(): void {
   }
@@ -14,5 +68,4 @@ export class ViewComponent implements OnDestroy, OnInit {
   ngOnInit(): void {
     console.log('oninit');
   }
-
 }
--- src/app/pages/warehouse/warehouse.module.ts
+++ src/app/pages/warehouse/warehouse.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {ViewComponent} from './view/view.component';
+import {NbCardModule, NbLayoutModule} from "@nebular/theme";
+import {Ng2SmartTableModule} from "ng2-smart-table";
 
 const routes: Routes = [{
   path: '',
@@ -16,6 +18,9 @@ const routes: Routes = [{
 @NgModule({
   imports: [
     RouterModule.forChild(routes),
+    NbLayoutModule,
+    NbCardModule,
+    Ng2SmartTableModule,
   ],
   declarations: [
     ViewComponent,
